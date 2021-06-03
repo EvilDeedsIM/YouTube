f03d0cb0f7db701c0ca739b5ef991074a06ba724
EvilDeedsIM <evildeedsim@gmail.com>
2021-06-03 07:47:39 +0300

    infinite_auto_carousel

---
+++ V_infinite_autoplay_carousel/app.js
@@ -0,0 +1,60 @@
+"use strict";
+
+function carousel() {
+  let carouselSlider = document.querySelector(".carousel__slider");
+  let list = document.querySelector(".carousel__list");
+  let item = document.querySelectorAll(".carousel__item");
+  let list2;
+
+  const speed = 1;
+
+  const width = list.offsetWidth;
+  let x = 0;
+  let x2 = width;
+
+  function clone() {
+    list2 = list.cloneNode(true);
+    carouselSlider.appendChild(list2);
+    list2.style.left = `${width}px`;
+  }
+
+  function moveFirst() {
+    x -= speed;
+
+    if (width >= Math.abs(x)) {
+      list.style.left = `${x}px`;
+    } else {
+      x = width;
+    }
+  }
+
+  function moveSecond() {
+    x2 -= speed;
+
+    if (list2.offsetWidth >= Math.abs(x2)) {
+      list2.style.left = `${x2}px`;
+    } else {
+      x2 = width;
+    }
+  }
+
+  function hover() {
+    clearInterval(a);
+    clearInterval(b);
+  }
+
+  function unhover() {
+    a = setInterval(moveFirst, 10);
+    b = setInterval(moveSecond, 10);
+  }
+
+  clone();
+
+  let a = setInterval(moveFirst, 10);
+  let b = setInterval(moveSecond, 10);
+
+  carouselSlider.addEventListener("mouseenter", hover);
+  carouselSlider.addEventListener("mouseleave", unhover);
+}
+
+carousel();
